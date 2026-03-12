bad9c32be118d77c935cd7f9abed61a8f951379c
yash1302 <jadhavyash623@gmail.com>
2026-03-12 09:25:55 +0530

    Day 3 : kth_smallest_element(using lamuto)

--- sort revision/kth_smallest_element.js
+++ sort revision/kth_smallest_element.js
@@ -4,30 +4,43 @@ let swap = (arr, low, high) => {
   arr[high] = temp;
 };
 
-let partition = (arr, low, high) => {
-  let pivot = arr[low];
-  let i = low - 1;
-  let j = high + 1;
-  while (true) {
-    do {
-      i++;
-    } while (arr[i] < pivot);
-    do {
-      j--;
-    } while (arr[j] > pivot);
-    if (i >= j) {
-        console.log(j,"j",arr,"arr")
+// let partition = (arr, low, high) => {
+//   let pivot = arr[low];
+//   let i = low - 1;
+//   let j = high + 1;
+//   while (true) {
+//     do {
+//       i++;
+//     } while (arr[i] < pivot);
+//     do {
+//       j--;
+//     } while (arr[j] > pivot);
+//     if (i >= j) {
+//         console.log(j,"j",arr,"arr")
 
-      return j;
+//       return j;
+//     }
+//     swap(arr, i, j);
+//   }
+// };
+
+let partition = (arr,low,high)=>{
+    let pivot = arr[high];
+    let i = low -1;
+    for(let j=low;j<=high-1;j++){
+        if(arr[j]<pivot){
+            i++;
+            swap(arr,i,j);
+        }
     }
-    swap(arr, i, j);
-  }
-};
+    swap(arr,i+1,high);
+    return (i+1);
+}
 
 let kth_ele = (arr, low, high, k) => {
   while (low <= high) {
     let p = partition(arr, low, high);
-    // console.log(arr,"arrrrrrr")
+    console.log(arr,"arrrrrrr",p,"p")
     if (p === k - 1) {
         console.log(arr[p],"kth smallest element")
       return p;
@@ -41,5 +54,5 @@ let kth_ele = (arr, low, high, k) => {
 
 let arr = [10, 4, 5, 8, 11, 6, 26];
 
-kth_ele(arr, 0, arr.length - 1, 5);
+kth_ele(arr, 0, arr.length - 1, 2);
 // console.log(arr,"arr ")
