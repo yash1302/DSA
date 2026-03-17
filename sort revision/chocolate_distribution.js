f9084dede3c4568054bb696a468417f666053db6
yash1302 <jadhavyash623@gmail.com>
2026-03-17 08:47:21 +0530

    DAY 4 : chocolate distribution

--- sort revision/chocolate_distribution.js
+++ sort revision/chocolate_distribution.js
@@ -5,7 +5,7 @@ let minDiff = (arr, n, m) => {
 
   arr.sort((a, b) => a - b);
   let res = arr[m - 1] - arr[0];
-  for (let i = 0; i + m - 1 < n; i++) {
+  for (let i = 1; i + m - 1 < n; i++) {
     res = Math.min(res, arr[i + m - 1] - arr[i]);
   }
   console.log(res, "res");
